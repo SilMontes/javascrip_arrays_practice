729e59828aff3f56c4d5e4f3ac840fe7a062e83c
SilMontes <Silmontes2001@gmail.com>
2021-02-19 15:15:32 +0000

    avance

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,10 +1,29 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
 
-// your code here
-for(var i=0; i<par.length; i++){
-    par.forEach (item, index) => {
-        if(par[i].includes("") )
-    }
+
+let separate = string => {
+    let words= string.split(" ");
+    return words.map( letter => letter[letter.length].toLowerCase());
 }
-console.log(counts);
+
+console.log(separate(par));
+
+
+
+
+// let separate = string => {
+//     let words = string.split(" ");
+//     return words;
+// } 
+
+// console.log(separate(par));
+
+// your code here
+// for(var i=0; i<par.length; i++){
+//     par.forEach (item, index) => {
+//         if(par[i].includes("") )
+//     }
+// }
+// console.log(counts);
+
